6528029e7ab43a0d321077b815268e4957c92bd5
FedorVeli4ko <fedor.velichko.2006@mail.ru>
2025-05-21 19:15:28 +0300

    начаты стили

--- quizlet/src/main/java/com/quizlet/Welcome.java
+++ quizlet/src/main/java/com/quizlet/Welcome.java
@@ -1,8 +1,30 @@
 package com.quizlet;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class Welcome {
+
+    private boolean darkTheme = true;
+
+    @FXML
+    private Button themeToggleButton;
+
+    private static Scene scene;
+
+
+    /*@FXML 
+    public void initialize(){
+        Platform.runLater(() -> 
+        {
+            this.scene = themeToggleButton.getScene();
+            scene.getStylesheets().add(getClass().getResource("scene.css").toExternalForm());
+        });
+        
+    }*/
 
     @FXML
     public void newQuiz() throws Exception {
@@ -17,5 +39,24 @@ public class Welcome {
     @FXML
     public void Statistic() throws Exception {
         App.setRoot("Statistics");
+    }
+
+    @FXML
+    private void toggleTheme() {
+        
+        this.scene = themeToggleButton.getScene();
+        if (scene == null) return;
+
+        scene.getStylesheets().clear();
+        if (darkTheme) {
+            System.out.println("toggleTheme");
+            
+            scene.getStylesheets().add(getClass().getResource("/com/quizlet/light-theme.css").toExternalForm());
+            themeToggleButton.setText("☀");
+        } else {
+            scene.getStylesheets().add(getClass().getResource("/com/quizlet/style.css").toExternalForm());
+            themeToggleButton.setText("🌙");
+        }
+        darkTheme = !darkTheme;
     }
 }
